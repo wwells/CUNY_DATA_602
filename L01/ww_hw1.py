abc05a848c5540fed80aa91f4a4318c3cf4bc577
wwells <walt@occ-data.org>
2017-02-01 08:59:02 -0600

    feat(L01): init

--- L01/ww_hw1.py
+++ L01/ww_hw1.py
@@ -1,19 +1,27 @@
 #1. fill in this function
 #   it takes a list for input and return a sorted version
 #   do this with a loop, don't use the built in list functions
+
 def sortwithloops(input):
-    return #return a value
+    l = []
+    #n = length(input)
+    for key in input:
+        l.append(key)
+    return l
 	
 #2. fill in this function
 #   it takes a list for input and return a sorted version
 #   do this with the built in list functions, don't us a loop
+
 def sortwithoutloops(input): 
-    return #return a value
+    input.sort()
+    return input
 
 #3. fill in this function
 #   it takes a list for input and a value to search for
 #	it returns true if the value is in the list, otherwise false
 #   do this with a loop, don't use the built in list functions
+
 def searchwithloops(input, value):
     return #return a value
 
@@ -21,15 +29,18 @@ def searchwithloops(input, value):
 #   it takes a list for input and a value to search for
 #	it returns true if the value is in the list, otherwise false
 #   do this with the built in list functions, don't use a loop
+
 def searchwithoutloops(input, value):
-    return #return a value	
+    return value in input  
+
+## TEST
 
 if __name__ == "__main__":	
     L = [5,3,6,3,13,5,6]	
 
-    print sortwithloops(L) # [3, 3, 5, 5, 6, 6, 13]
-    print sortwithoutloops(L) # [3, 3, 5, 5, 6, 6, 13]
-    print searchwithloops(L, 5) #true
-	#print searchwithloops(L, 11) #false
-    #print searchwithoutloops(L, 5) #true
-	#print searchwithoutloops(L, 11) #false
+    print "sortwithLoops 1: \n %s" %  sortwithloops(L) # [3, 3, 5, 5, 6, 6, 13]
+    print "sortwithoutLoops 2: \n %s" % sortwithoutloops(L) # [3, 3, 5, 5, 6, 6, 13]
+    #print "searchwithloops 3a: \n %s" % searchwithloops(L, 5) #true
+	#print "searchwithloops 3b: \n %s" % searchwithloops(L, 11) #false
+    print "searchwithoutloops 4a: \n %s" % searchwithoutloops(L, 5) #true
+    print "searchwithoutloops 4b: \n %s" % searchwithoutloops(L, 11) #false
