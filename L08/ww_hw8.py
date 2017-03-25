06bb0ad84ed0406784fb16d83cf0c0c6efa30bba
wwells <walt@occ-data.org>
2017-03-25 13:27:54 -0500

    feat(HW8): fin

--- L08/ww_hw8.py
+++ L08/ww_hw8.py
@@ -29,9 +29,15 @@ the objects and midpoints for each image.  Remember, the focus here is to use re
 available Python functions to do image processing rather than gain a deep understanding 
 of the theory of the techniques.   
 
+References: 
+http://pythonvision.org/basic-tutorial/
+http://www-ee.uta.edu/Online/Devarajan/ee6358/BinaryImage.pdf
+http://www.scipy-lectures.org/advanced/image_processing/
+
 """
 import scipy.ndimage as ndimage
 import scipy.misc as misc
+import mahotas as mh
 
 
 class ImageCount:
@@ -43,18 +49,36 @@ class ImageCount:
 
 	def threshold(self, value, outfile, save=True):
 		"""use scipy gaussian_filter to convert a greyscale image to B&W based on threshold"""
+		
 		self.value = value
 		self.outfile = outfile
 		self.img = ndimage.gaussian_filter(self.raw, self.value)
-		self.thres = self.img > self.img.mean()
-		if save: misc.imsave(self.outfile, self.thres)
+		self.T = mh.thresholding.otsu(self.img)
+		self.result = self.img > self.T
+		
+		if save: misc.imsave(self.outfile, self.result)
 
+	def count_and_measure(self):
+		"""find number of white obj in converted image, count center"""
 
+		self.labels, self.count = mh.label(self.result)
+		print 'Image: %s | Number of Objects: %d ' % (self.file, self.count)
 
-
+		for i in range(1,self.count+1):
+			self.center = ndimage.measurements.center_of_mass(self.result, self.labels, i)
+			print 'Object %s with center at: %s ' % (i,  self.center[0:2])
+		print ""
 
 
 if __name__ == '__main__':
 	objects = ImageCount('objects.png')
-	objects.threshold(1, 'objects_BW.png')
+	objects.threshold(3, 'objects_BW.png')
+	objects.count_and_measure()
 
+	circles = ImageCount('circles.png')
+	circles.threshold(10, 'circles_BW.png')
+	circles.count_and_measure()
+
+	peppers = ImageCount('peppers.png')
+	peppers.threshold(3, 'peppers_BW.png')
+	peppers.count_and_measure()
